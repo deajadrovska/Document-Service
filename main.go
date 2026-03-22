98edafaf75a579c862dadfb72aee9eafd41b7d13
deajadrovska <jadrovska.dea@gmail.com>
2026-03-22 17:38:08 +0100

    Added main

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"Document-Service/internal/handler"
+	"Document-Service/internal/repository"
+	"Document-Service/internal/service"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	rep := repository.NewDocumentRepository()
+	ser := service.NewDocumentService(rep)
+	h := handler.NewDocumentHandler(ser)
+
+	router := gin.Default()
+	router.GET("/documents", h.GetDocuments)
+	router.GET("/documents/:id", h.GetDocumentById)
+	router.POST("/documents", h.AddDocument)
+	router.DELETE("/documents/:id", h.DeleteDocument)
+
+	err := router.Run(":7070")
+	if err != nil {
+		return
+	}
 }
